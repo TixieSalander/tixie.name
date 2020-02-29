27f76da16ae4723045e346e034c89261cb4132ac
Tixie <contact@tixie.name>
2020-02-29 16:24:36 +0100

    refactor(now page): Reduce lightbox instances to one

--- assets/js/now-gallery.js
+++ assets/js/now-gallery.js
@@ -1,19 +1,14 @@
 class NowGallery {
   constructor() {
-    this.containers = document.querySelectorAll('[data-now-gallery]')
-    this.selector = '[data-now-gallery-item]'
-    this.gallery = {}
+    this.items = '[data-now-gallery]'
+    this.gallery = null
   }
 
   init () {
-    [].forEach.call(this.containers, item => {
-      const galleryIndex = item.getAttribute('data-now-gallery')
-      const itemSelectors = `[data-now-gallery="${ galleryIndex }"] [data-now-gallery-item]`
-      this.gallery[galleryIndex] = new Tobi({
-        selector: itemSelectors,
-        captions: false,
-        zoom: false,
-      })
+    this.gallery = new Tobi({
+      selector: this.items,
+      captions: false,
+      zoom: false
     })
   }
 }
